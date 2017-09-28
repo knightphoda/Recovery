3b0e4a40a4097c8ce706f7ea51cd8aeaddd958e2
Luis <lu.gustavo.lima@hotmail.com>
2017-09-28 19:49:50 -0300

    enviei o projeto todo

---
+++ JavaApplicationBruno/src/javaapplicationbruno/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationbruno;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Luis
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        /*JFrame frame = new JFrame("Recovery  Fisioterapia e Osteopatia");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit t = Toolkit.getDefaultToolkit();
+        Dimension dimensao = t.getScreenSize();
+        frame.setSize((dimensao.width + 5), (dimensao.height - 38));
+        frame.setVisible(true);
+        ImageIcon img = new ImageIcon("icone.png"); //NAO FUNCIONA
+        frame.setIconImage(img.getImage());         //NAO FUNCIONA
+        */
+        
+        //frame.dispose();
+    }
+    
+}
